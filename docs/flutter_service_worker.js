d6c7c34b9689493742a2a2473ae1fc086ca417ca
kosuke-lab <91716913+Matsui-Kosuke@users.noreply.github.com>
2024-10-29 08:34:24 +0900

    日時データの保存対応

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ad177e4aa519f5c5e1cc01563cb6c000",
+const RESOURCES = {"flutter_bootstrap.js": "7e9fa75ce0feba7612d807b0e1d8be8b",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7",
 "index.html": "aaa225bbb48d4817267a55f77309de40",
 "/": "aaa225bbb48d4817267a55f77309de40",
-"main.dart.js": "150c5cfc4dcb3fef6c152e9655547992",
+"main.dart.js": "24b24adededbef3e2744c151a39b19bc",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
