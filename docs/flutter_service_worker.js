6936fee933f05a95eca1395bed34eefbd56632af
kosuke-lab <91716913+Matsui-Kosuke@users.noreply.github.com>
2024-11-09 01:22:15 +0900

    バックアップ機能の修正

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b299c22d5347880875e509cff9a5ef68",
+const RESOURCES = {"flutter_bootstrap.js": "ae96f99675c9cdb81fab33a0fa717872",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7",
 "index.html": "aaa225bbb48d4817267a55f77309de40",
 "/": "aaa225bbb48d4817267a55f77309de40",
-"main.dart.js": "0ea599d93acd915def6d5c8b59c6abc6",
+"main.dart.js": "f90129dcb3f46f163acb8351ffd6f8ac",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
