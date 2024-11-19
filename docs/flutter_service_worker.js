80ef46138dfb4434e59f8a6a736337cc1d71bf9c
kosuke-lab <91716913+Matsui-Kosuke@users.noreply.github.com>
2024-11-19 23:12:04 +0900

    15:15まで縄跳び画像15:30までやせ画像に修正

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3469bab7c84aea24d6f9bf3e0ab7f1e1",
+const RESOURCES = {"flutter_bootstrap.js": "5e54ff29a1efc74dba1370726e4a3c55",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7",
 "index.html": "aaa225bbb48d4817267a55f77309de40",
 "/": "aaa225bbb48d4817267a55f77309de40",
-"main.dart.js": "7e3e6c20ed17a6738d7ae3ba9161fa70",
+"main.dart.js": "770cc9cc2493c017ce1ca7030a14731b",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
